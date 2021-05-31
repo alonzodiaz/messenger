9058b1c57ad8d1a8356ec7b4e0cc17f3fe7c9830
Alonzo Diaz <ad81@rice.edu>
2021-05-31 16:06:30 -0500

    client and server architecture now

--- src/main/view/MainView.java
+++ src/main/view/MainView.java
@@ -1,11 +1,34 @@
 package main.view;
 
-public class MainView {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainView extends JFrame {
+  // Overall pane
+  private JPanel contentPane;
+
+
+
   public MainView() {
-    
+    initGUI();
+  }
+
+  private void initGUI() {
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setBounds(100, 100, 1500, 500);
+    contentPane = new JPanel();
+    setContentPane(contentPane);
+
+
   }
 
   public void start() {
+    this.setVisible(true);
     System.out.println("Main View Start");
   }
 }
